d9e9c0b1a61b84f6f75637963df5c3b680f1dc38
shanacon <n107502530@office365.ncu.edu.tw>
2022-07-06 21:05:20 +0800

    complete first part of .SECT

--- _main.py
+++ _main.py
@@ -1,13 +1,13 @@
-class Case :
+class COLUMN :
     def __init__(self, name, BC, HC, No1, No, Numx, Numy, S, SM, whichFloor):
         self.name = name
-        tmp = whichFloor[:-1]
-        if tmp[0] == 'R':
-            self.FloorPtr = int(tmp[1:]) + 1000
-        elif tmp[0] == 'B':
-            self.FloorPtr = int(tmp[1:]) * -1
-        elif IsInt(tmp) :
-            self.FloorPtr = int(tmp)
+        self.RCMaterial = name + '_CONC'
+        if whichFloor[0] == 'R':
+            self.FloorPtr = int(whichFloor[1:]) + 1000
+        elif whichFloor[0] == 'B':
+            self.FloorPtr = int(whichFloor[1:]) * -1
+        elif IsInt(whichFloor) :
+            self.FloorPtr = int(whichFloor)
         else :
             self.FloorPtr = -1000
         self.type = type  
@@ -20,9 +20,143 @@ class Case :
         self.S = S
         self.SM = SM
         self.whichFloor = whichFloor
+class BEAM :
+    def __init__(self, name, BC, HC, No, S, SM, whichFloor):
+        self.name = name
+        self.RCMaterial = name + '_CONC'
+        if whichFloor[0] == 'R':
+            if len(whichFloor) == 1:
+                self.FloorPtr = 1000
+            elif whichFloor[-1] >= '0' and whichFloor[-1] <= '9' :
+                self.FloorPtr = int(whichFloor[1:]) + 1000
+            else :
+                self.FloorPtr = int(whichFloor[1:-1]) + 1000
+        elif whichFloor[0] == 'B':
+            self.FloorPtr = int(whichFloor[1:]) * -1
+        elif IsInt(whichFloor) :
+            self.FloorPtr = int(whichFloor)
+        else :
+            self.FloorPtr = -1000
+        self.type = type  
+        self.BC = BC  
+        self.HC = HC  
+        self.No = No  
+        self.S = S
+        self.SM = SM
+        self.whichFloor = whichFloor
 def IsInt(s):
     try: 
         int(s)
         return True
     except ValueError:
         return False
+def compare(case):
+    return -case.FloorPtr
+def defaultList(InList, phase) :
+    if phase == 0 :
+        InList.append('$Unit\n')
+        InList.append('KGF-CM\n')
+        InList.append('\n')
+        InList.append('$ RC Rectangle Section Definitions\n')
+        InList.append('$\tName\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tAngle\n')
+        InList.append('\t\t\t\tcm\tcm\tcm\n')
+    elif phase == 1 :
+        InList.append('\n$ End RC Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ RC Circle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tDiameter\tCover\tSNo\tSpacing\tSpacingM\tAngle\n')
+        InList.append(' \t\t\tcm\t\tcm\t\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End RC Circle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ RC Hollow Rectangle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tth\ttw\tAngle\n')
+        InList.append(' \t\t\tcm\tcm\tcm\tcm\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End RC Hollow Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ Composite A Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tth\tAngle\n')
+        InList.append(' \t\t\tcm\tcm\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End Composite A Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ Composite B Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tth\tAngle\n')
+        InList.append(' \t\t\tcm\tcm\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End Composite B Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ Composite Right Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tth\tAngle\n')
+        InList.append(' \t\t\tcm\tcm\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End Composite Right Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ SJS Retrofit Circle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tSMaterial\tDiameter\tCover\tSNo\tSpacing\tSpacingM\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\t\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End SJS Retrofit Circle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ SJS Retrofit Rectangle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tSMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End SJS Retrofit Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ SJR Retrofit Circle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tSMaterial\tDiameter\tCover\tSNo\tSpacing\tSpacingM\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\t\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End SJR Retrofit Circle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ SJR Retrofit Rectangle Section Definitions\n')
+        InList.append('$ Name\tRCMaterial\tSMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End SJR Retrofit Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ CJS Retrofit Circle Section Definitions\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tDiameter\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\t\tcm\t\t\tcm\t\tcm\t\tcm\t\tcm\n')
+        InList.append('\n')
+        InList.append('$ End CJS Retrofit Circle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ CJS Retrofit Rectangle Section Definitions\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tWidth\tHeight\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\t\tcm\t\tcm\t\tcm\t\tcm\n')
+        InList.append('\n')
+        InList.append('$ End CJS Retrofit Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ CJR Retrofit Circle Section Definitions\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tDiameter\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\t\tcm\t\t\tcm\t\tcm\t\tcm\t\tcm\n')
+        InList.append('\n')
+        InList.append('$ End CJR Retrofit Circle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ CJR Retrofit Rectangle Section Definitions\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tWidth\tHeight\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tt\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\t\tcm\t\tcm\t\tcm\t\tcm\n')
+        InList.append('\n')
+        InList.append('$ End CJR Retrofit Rectangle Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ WingWall A Section Definitons\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tWidth\tHeight\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tWL\tWR\tHU\tHB\tTSteel\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\t\t\tcm\tcm\t\tcm\t\tcm\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End WingWall A Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ DUF A Section Definitons\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tWidth\tHeight\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tWR\tHU\tHB\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\t\t\tcm\tcm\t\tcm\t\tcm\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End DUF A Section Definitions\n')
+        InList.append('\n')
+        InList.append('$ DUF B Section Definitons\n')
+        InList.append('$ Name\tRCMaterialA\tRCMaterialB\tWidth\tHeight\tCover\tSNoA\tSNoB\tSpacingA\tSpacingM\tSpacingB\tWL\tWR\tHU\tAngle\n')
+        InList.append(' \t\t\t\t\tcm\tcm\tcm\t\t\t\tcm\tcm\t\tcm\t\tcm\tcm\tcm\tcm\n')
+        InList.append('\n')
+        InList.append('$ End DUF B Section Definitions\n\n')
+        InList.append('$ Steels Location\n')
+        InList.append('$\tName\t\tMaterial\t\tNo. X Y\n')
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from email.policy import default
 import os
-from tkinter import ALL
+
+from numpy import spacing
 from _main import *
 from LogSystem import *
-import time
 
 cwd = os.getcwd()
 filename = cwd.split('\\')[-1]
-OutputData = []
-OutputData.append('$Unit\n')
-OutputData.append('KGF-CM\n')
-OutputData.append('\n')
-OutputData.append('$ RC Rectangle Section Definitions\n')
-OutputData.append('$\tName\tRCMaterial\tWidth\tHeight\tCover\tSNo\tSpacing\tSpacingM\tAngle\n')
-OutputData.append('\t\t\t\tcm\tcm\tcm')
+OutputDataX = []
+defaultList(OutputDataX, 0)
+OutputDataY = []
+defaultList(OutputDataY, 0)
 # load CXX
 CXX = ReadFile('CXX.DAT', os.path.basename(__file__))
 ## get Floor and Cross section
@@ -35,8 +32,6 @@ CXX_Data = CXX.readlines()
 LineLen = len(CXX_Data)
 Progress = 0
 CaseCXX = 0
-NowC = 0
-NowF = 0
 while CaseCXX < LineLen:
     ## set lines
     lines = []
@@ -53,9 +48,9 @@ while CaseCXX < LineLen:
     #floor and name
     try :
         if lines[0][0][-1] >= '0' and lines[0][0][-1] <= '9' or lines[0][0][-1] == 'F':
-            floor = lines[0][0] + 'F'
+            floor = lines[0][0]
         else :
-            floor = lines[0][0][:-1] + 'F'
+            floor = lines[0][0][:-1]
         name = floor + lines[0][2]
     except Exception as e:
         WriteEx()
@@ -90,7 +85,7 @@ while CaseCXX < LineLen:
         WriteEx()
         ExceptionExit('CXX: line 4 or 5 Out of range. Doing No Numxy S.')
     whichFloor = floor
-    ALLCOLUMN.append(Case(name, BC, HC, No1, No, Numx, Numy, S, SM, whichFloor))
+    ALLCOLUMN.append(COLUMN(name, BC, HC, No1, No, Numx, Numy, S, SM, whichFloor))
     CaseCXX = CaseCXX + 6
     ## progress bar
     # if float(CaseCXX / LineLen) * 10.0 > Progress:
@@ -104,8 +99,86 @@ while CaseCXX < LineLen:
     #     print(']', end = '')
     #     time.sleep(0.05)
     ## progress bar
-for item in ALLCOLUMN :
-    print(item.name)
-output = open(filename + '.SECT', mode = 'w')
-for line in OutputData :
-    output.write(line)
+#load beam data
+Beam = ReadFile('A6B103C.dat', os.path.basename(__file__))
+#load useless data
+KeepRead  = True
+while KeepRead:
+    line = Beam.readline()
+    if line.find('BEAM') != -1:
+        KeepRead = False
+KeepRead  = True
+while KeepRead:
+    line = Beam.readline()
+    if line != '\n':
+        KeepRead = False
+ALLBEAM = []
+BEAM_Data = [line]
+for line in Beam.readlines():
+    if line != '\n' and line.find('BEAM') == -1:
+        BEAM_Data.append(line)
+LineLen = len(BEAM_Data)
+Progress = 0
+CaseBeam = 0
+while CaseBeam < LineLen:
+    ## set lines
+    lines = []
+    try :
+        lines.append(BEAM_Data[CaseBeam])
+        lines.append(BEAM_Data[CaseBeam + 1])
+        lines.append(BEAM_Data[CaseBeam + 2])
+        lines.append(BEAM_Data[CaseBeam + 3])
+        lines.append(BEAM_Data[CaseBeam + 4])
+        lines.append(BEAM_Data[CaseBeam + 5])
+        lines.append(BEAM_Data[CaseBeam + 6])
+        lines.append(BEAM_Data[CaseBeam + 7])
+    except Exception as e:
+        WriteEx()
+        ExceptionExit('BEAMData Out of range.')
+    ## get case num of these line
+    Casenum = lines[0].count('*') - 1
+    ## get name and BC, HC
+    nameList = []
+    BCList = []
+    HCList = []
+    SNoList = []
+    SList = []
+    SMList = []
+    WFList = []
+    tmp = lines[0].split('*')
+    tmp6 = lines[6].split()
+    STIRcount = 2
+    for i in range(1, 1 + Casenum) :
+        tmp[i] = tmp[i].replace('(', ' ')
+        tmp[i] = tmp[i].replace(')', ' ')
+        tmp[i] = tmp[i].replace('x', ' ')
+        tmp[i] = tmp[i].replace('/', ' ')
+        data = tmp[i].split()
+        nameList.append(data[0] + data[1])
+        WFList.append(data[0])
+        BCList.append(data[2])
+        HCList.append(data[3])
+        SNoList.append('#' + tmp6[STIRcount].split('#')[-1])
+        SList.append(str(max(float(tmp6[STIRcount + 1]),float(tmp6[STIRcount + 3]))))
+        SMList.append(tmp6[STIRcount + 2])
+        STIRcount = STIRcount + 4
+    for i in range(Casenum) :
+        if nameList[i].find('P') == -1:
+            ALLBEAM.append(BEAM(nameList[i], BCList[i], HCList[i], SNoList[i], SList[i], SMList[i], WFList[i]))
+    CaseBeam = CaseBeam + 8
+ALLCOLUMN.sort(key=compare)
+ALLBEAM.sort(key=compare)
+for column in ALLCOLUMN :
+    OutputDataX.append(f'\t{column.name}\t{column.RCMaterial}\t{column.HC}\t{column.BC}\t5\t{column.No}\t{column.S}\t{column.SM}\t\t0\n')
+    OutputDataY.append(f'\t{column.name}\t{column.RCMaterial}\t{column.BC}\t{column.HC}\t5\t{column.No}\t{column.S}\t{column.SM}\t\t0\n')
+for beam in ALLBEAM:
+    OutputDataX.append(f'\t{beam.name}\t{beam.RCMaterial}\t{beam.BC}\t{beam.HC}\t5\t{beam.No}\t{beam.S}\t{beam.SM}\t\t0\n')
+    OutputDataY.append(f'\t{beam.name}\t{beam.RCMaterial}\t{beam.HC}\t{beam.BC}\t5\t{beam.No}\t{beam.S}\t{beam.SM}\t\t0\n')
+defaultList(OutputDataX, 1)
+defaultList(OutputDataY, 1)
+outputX = open(filename + '+X.SECT', mode = 'w')
+for line in OutputDataX :
+    outputX.write(line)
+outputY = open(filename + '+Y.SECT', mode = 'w')
+for line in OutputDataY :
+    outputY.write(line)
